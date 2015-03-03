503c7709670356693c1c78904b4f4f2f63db10b6
rmarquez1 <albertocomp10@gmail.com>
2015-03-02 22:39:14 -0430

    Consultar pacientes

--- Versión MOV/Código/HOI/paciente_consulta.php
+++ Versión MOV/Código/HOI/paciente_consulta.php
@@ -23,6 +23,76 @@
         <link rel="stylesheet" type="text/css" href="./css/basico.css"/>    </head>
         <?php session_start(); ?>
     <body>
+
+        <?php
+        
+            include_once ("fachadaBD.php");
+            include_once ("Paciente.php");
+
+
+            function test_input($data) {
+               $data = trim($data);
+               $data = stripslashes($data);
+               $data = htmlspecialchars($data);
+               return $data;
+            }
+            function verificar_campos(){
+                $valido = true;
+                if((trim($_POST["ci"])) == "" ){
+                    $valido = false;
+                }
+                return $valido;
+            }
+
+            /*  Funcion que se encarga de realizar la busqueda en la base de datos a partir de
+                la cedula del paciente e imprime los resultados en pantalla.
+            */
+            function realizarBusqueda(){
+
+                $cedula = trim($_POST["ci"]);
+
+                $fbd = fachadaBD::getInstance();
+                $resultado = $fbd->consultarPacienteBD($cedula);
+                $paciente = new Paciente();
+
+                if (($row = oci_fetch_array($resultado, OCI_BOTH))){
+                    $paciente->setCI($row[0]);
+                    $paciente->setNombres($row[1]);
+                    $paciente->setApellidos($row[2]);
+                    $paciente->setProfesion($row[3]);
+                    $paciente->setLugarRes($row[4]);
+                    $paciente->setFechaNac($row[5]);
+                    $paciente->setID_Historial($row[6]);
+                    $paciente->setDiagnostico($row[7]);
+                    $paciente->setInterQuir($row[8]);
+                }
+
+                return $paciente;   
+            }
+
+            $check = false;
+            $verificarCampos = true;
+            $esta = true;
+            $valido = true;
+
+            
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $verificarCampos = verificar_campos();
+
+                if ($verificarCampos){
+                    $paciente = realizarBusqueda();
+                    if ($paciente==null){ 
+                        $esta = false;
+                    }else {
+                        $check = true;
+                        $esta = true;
+                    }
+                }
+                  
+            }
+        
+        ?>
+
     	<div class="container-fluid" style="height: 100%;">
     		<!-- TOPBAR -->
     		<div class="row topbar">
@@ -78,7 +148,45 @@
 	    				<!-- CONTENT -->
 	    				<div class="row">
 	    					<div class="col-xs-12 content">
-	    						<h3 class="title">Buscar pacientes</h3>
+	    						<form class="form-horizontal" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                                    <!-- DATOS DEL PACIENTE -->
+                                    <h3 class="title">Buscar Pacientes</h3>
+                                    <div class="form-group">
+                                        <label class="col-xs-2 control-label">Cédula</label>
+                                        <div class="col-xs-10">
+                                            <input type="text" class="form-control" placeholder="Cédula del paciente" name="ci">
+                                        </div>
+                                    </div>
+                                    <?php if (!$verificarCampos): ?>
+                                        <div class="alert alert-danger error" role="alert">
+                                            <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                                            <span class="sr-only">Error:</span>
+                                            <?php echo "Escriba la cédula del paciente que desea consultar";?>
+                                        </div>                            
+                                    <?php endif; ?>
+                                    <?php if (!$esta): ?>
+                                        <div class="alert alert-danger error" role="alert">
+                                            <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                                            <span class="sr-only">Error:</span>
+                                            <?php echo "NO existe!!";?>
+                                        </div>      
+                                    <?php endif; ?>
+                                    <?php if ($check): ?>
+                                        <ul>
+                                            <li> Nombres: <?php echo $paciente->getNombres();?></li>
+                                            <li> Apellidos: <?php echo $paciente->getApellidos();?></li>
+                                            <li> Identificación: <?php echo $paciente->getCI();?></li>
+                                        </ul>   
+                                    <?php endif; ?>                        
+                                    
+                                    
+                                    
+                                    <div style="text-align: center; margin-top: 30px;">
+                                        <button type="submit" class="btn btn-primary">Buscar</button>
+                                        <button type="button" class="btn btn-default" onClick="document.location.href='<?php echo "paciente_consulta.php" ?>'">Cancelar</button>
+                                        </div>
+                                    </div>
+                                </form>  
 								
                                 <h3 class="title">Consulta general</h3>
 	    					</div>
